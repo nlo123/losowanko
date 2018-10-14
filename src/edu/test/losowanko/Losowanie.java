a2be82565ebaaf8e44d773bd0d59a76a5b896de6
nlo123 <norbert@debian-hp>
2018-10-14 11:36:12 +0200

    Dodanie klasy Losowanie

---
+++ src/edu/test/losowanko/Losowanie.java
@@ -0,0 +1,20 @@
+package edu.test.losowanko;
+
+import java.util.Random;
+
+public class Losowanie {
+
+    public static void main(String[] args) {
+        
+        for(int j=0; j<1000000; j++) {
+            Random rn = new Random();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<4; i++) {
+                sb.append(rn.nextInt(10));
+            }
+            System.out.println(sb.toString());
+        }
+
+    }
+
+}
