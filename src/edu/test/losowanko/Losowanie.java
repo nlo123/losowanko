f82362206517f1a13b8e39e4dc6e01392d19a76d
nlo123 <norbert@debian-hp>
2018-10-14 11:38:00 +0200

    Dodanie pliku .gitignore

--- src/edu/test/losowanko/Losowanie.java
+++ src/edu/test/losowanko/Losowanie.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Losowanie {
 
     public static void main(String[] args) {
-        
+
         for(int j=0; j<1000000; j++) {
             Random rn = new Random();
             StringBuilder sb = new StringBuilder();
