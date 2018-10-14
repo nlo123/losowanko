b3a6f7e2297bf984d67ff7464fcc79388e49ccc0
nlo123 <norbert@debian-hp>
2018-10-14 11:44:17 +0200

    Losowanie liczb

--- src/edu/test/losowanko/Losowanie.java
+++ src/edu/test/losowanko/Losowanie.java
@@ -5,16 +5,17 @@ import java.util.Random;
 public class Losowanie {
 
     public static void main(String[] args) {
-//
+        int mniejsze=0, wieksze=0;
         for(int j=0; j<1000000; j++) {
             Random rn = new Random();
             StringBuilder sb = new StringBuilder();
             for(int i=0; i<4; i++) {
                 sb.append(rn.nextInt(10));
             }
-            System.out.println(sb.toString());
+            if(Integer.parseInt(sb.toString()) > 500) wieksze++; else mniejsze++;
         }
-
+        System.out.println("Większe: " + wieksze);
+        System.out.println("Mniejsze: " + mniejsze);
     }
 
 }
